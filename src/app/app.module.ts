0d81115950fa33935b7e9ba89b4ab201a89e26f3
J4mJeMax <J4Mjemax@gmail.com>
2020-05-09 01:54:32 +0200

    made downloading data from API + added single post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { SinglePostComponent } from './single-post/single-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    SinglePostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-posts',
@@ -7,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+    postsLoading = true;
+    wordpressPosts: any;
+    error: boolean;
 
-  ngOnInit() {}
+    getWPPosts() {
+        this.http.get('https://public-api.wordpress.com/rest/v1.1/sites/en.blog.wordpress.com/posts', {}).subscribe({
+            next: data => {
+                console.log(data);
+                this.wordpressPosts = data;
+                this.postsLoading = false;
+            },
+            error: () => this.error = true
+        });
+    }
+
+  ngOnInit() {
+        this.getWPPosts();
+  }
 
 }
