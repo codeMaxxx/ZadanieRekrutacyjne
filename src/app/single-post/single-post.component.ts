1cb5b672102bee546b2460e224357198faff798f
J4mJeMax <J4Mjemax@gmail.com>
2020-05-09 02:52:44 +0200

    updated view + made function that allow to download 1 post

--- src/app/single-post/single-post.component.ts
+++ src/app/single-post/single-post.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
-  styleUrls: ['./single-post.component.css']
+  styleUrls: ['./single-post.component.css'],
 })
 export class SinglePostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  activePostData: any;
+
+  getPost(id) {
+    this.activePostData = {};
+    this.http.get(`https://public-api.wordpress.com/rest/v1.1/sites/en.blog.wordpress.com/posts/${id}`, {}).subscribe({
+      next: data => {
+        this.activePostData = data;
+      },
+    });
+  }
 
   ngOnInit() {
   }
