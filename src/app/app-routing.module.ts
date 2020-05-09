c67c554cd6171d28b3b3403bd13838c6a2b50d9d
J4mJeMax <J4Mjemax@gmail.com>
2020-05-09 14:33:36 +0200

    Changed views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SinglePostComponent} from './single-post/single-post.component';
+import {PostsComponent} from './posts/posts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'post/:id', component: SinglePostComponent},
+  {path: 'posts', component: PostsComponent},
+  {path: '', redirectTo: 'posts', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/single-post/single-post.component.ts
+++ src/app/single-post/single-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-single-post',
@@ -8,20 +9,39 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 })
 export class SinglePostComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  id: number;
   activePostData: any;
-
+  comments: any;
+  isPostActive = false;
   getPost(id) {
-    this.activePostData = {};
     this.http.get(`https://public-api.wordpress.com/rest/v1.1/sites/en.blog.wordpress.com/posts/${id}`, {}).subscribe({
       next: data => {
+        console.log(data);
         this.activePostData = data;
+        this.isPostActive = true;
       },
     });
   }
 
+  // getPostComment(id) {
+  //   this.http.get(`https://public-api.wordpress.com/rest/v1.1/sites/en.blog.wordpress.com/comments/${id}`, {}).subscribe({
+  //     next: data => {
+  //       console.log(data);
+  //       this.comments = data;
+  //     },
+  //   });
+  // }
+
+
+
   ngOnInit() {
+
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+    this.getPost(this.id);
+    // this.getPostComment(this.id);
   }
 
 }
